2677bfb007135cea009989ebb5a1da4a426b7837
Zhuyi Xue <alfred532008@gmail.com>
2018-03-28 16:26:08 -0700

    better formatted 04_word2vec.py

--- assignments/01/04_word2vec.py
+++ assignments/01/04_word2vec.py
@@ -43,36 +43,51 @@ def word2vec(dataset):
     In word2vec, it's actually the weights that we care about 
     """
     with tf.name_scope('embed'):
-        embed_matrix = tf.get_variable('embed_matrix', 
-                                        shape=[VOCAB_SIZE, EMBED_SIZE],
-                                        initializer=tf.random_uniform_initializer())
-        embed = tf.nn.embedding_lookup(embed_matrix, center_words, name='embedding')
+        embed_matrix = tf.get_variable(
+            'embed_matrix',
+            shape=[VOCAB_SIZE, EMBED_SIZE],
+            initializer=tf.random_uniform_initializer()
+        )
+        embed = tf.nn.embedding_lookup(
+            embed_matrix, center_words, name='embedding')
 
     # Step 4: construct variables for NCE loss and define loss function
     with tf.name_scope('loss'):
-        nce_weight = tf.get_variable('nce_weight', shape=[VOCAB_SIZE, EMBED_SIZE],
-                        initializer=tf.truncated_normal_initializer(stddev=1.0 / (EMBED_SIZE ** 0.5)))
-        nce_bias = tf.get_variable('nce_bias', initializer=tf.zeros([VOCAB_SIZE]))
+        nce_weight_init = tf.truncated_normal_initializer(
+            stddev=1.0 / (EMBED_SIZE ** 0.5))
+        nce_weight = tf.get_variable(
+            'nce_weight',
+            shape=[VOCAB_SIZE, EMBED_SIZE],
+            initializer=nce_weight_init)
+        nce_bias = tf.get_variable(
+            'nce_bias',
+            initializer=tf.zeros([VOCAB_SIZE]))
 
         # define loss function to be NCE loss function
-        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weight, 
-                                            biases=nce_bias, 
-                                            labels=target_words, 
-                                            inputs=embed, 
-                                            num_sampled=NUM_SAMPLED, 
-                                            num_classes=VOCAB_SIZE), name='loss')
+        raw_loss = tf.nn.nce_loss(
+            weights=nce_weight,
+            biases=nce_bias,
+            labels=target_words,
+            inputs=embed,
+            num_sampled=NUM_SAMPLED,
+            num_classes=VOCAB_SIZE
+        )
+        loss = tf.reduce_mean(raw_loss, name='loss')
 
     # Step 5: define optimizer
     with tf.name_scope('optimizer'):
-        optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
-    
+        optimizer = tf.train.GradientDescentOptimizer(
+            LEARNING_RATE).minimize(loss)
+
     utils.safe_mkdir('checkpoints')
 
     with tf.Session() as sess:
         sess.run(iterator.initializer)
         sess.run(tf.global_variables_initializer())
 
-        total_loss = 0.0 # we use this to calculate late average loss in the last SKIP_STEP steps
+        # we use this to calculate late average loss in the last SKIP_STEP
+        # steps
+        total_loss = 0.0
         writer = tf.summary.FileWriter('graphs/word2vec_simple', sess.graph)
 
         for index in range(NUM_TRAIN_STEPS):
@@ -80,21 +95,34 @@ def word2vec(dataset):
                 loss_batch, _ = sess.run([loss, optimizer])
                 total_loss += loss_batch
                 if (index + 1) % SKIP_STEP == 0:
-                    print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
+                    print('Average loss at step {}: {:5.1f}'.format(
+                        index, total_loss / SKIP_STEP))
                     total_loss = 0.0
             except tf.errors.OutOfRangeError:
                 sess.run(iterator.initializer)
         writer.close()
 
+
 def gen():
-    yield from word2vec_utils.batch_gen(DOWNLOAD_URL, EXPECTED_BYTES, VOCAB_SIZE, 
-                                        BATCH_SIZE, SKIP_WINDOW, VISUAL_FLD)
+    yield from word2vec_utils.batch_gen(
+        DOWNLOAD_URL,
+        EXPECTED_BYTES,
+        VOCAB_SIZE,
+        BATCH_SIZE,
+        SKIP_WINDOW,
+        VISUAL_FLD
+    )
+
 
 def main():
-    dataset = tf.data.Dataset.from_generator(gen, 
-                                (tf.int32, tf.int32), 
-                                (tf.TensorShape([BATCH_SIZE]), tf.TensorShape([BATCH_SIZE, 1])))
+    dataset = tf.data.Dataset.from_generator(
+        gen,
+        (tf.int32, tf.int32), 
+        (tf.TensorShape([BATCH_SIZE]),
+         tf.TensorShape([BATCH_SIZE, 1]))
+    )
     word2vec(dataset)
+
 
 if __name__ == '__main__':
     main()
