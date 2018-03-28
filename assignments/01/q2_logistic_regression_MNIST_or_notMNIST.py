522edfcffc6186ffdf1c49629d6b18e5b0e2f174
Zhuyi Xue <alfred532008@gmail.com>
2018-03-28 14:17:08 -0700

    renamed 03_logreg_starter.py => q2_logistic_regression_MNIST_or_notMNIST.py

--- assignments/01/q2_logistic_regression_MNIST_or_notMNIST.py
+++ assignments/01/q2_logistic_regression_MNIST_or_notMNIST.py
@@ -7,13 +7,14 @@ cs20.stanford.edu
 Lecture 03
 """
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
-import numpy as np
+import sys
 import tensorflow as tf
 import time
 
 import utils
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 # Define paramaters for the model
 learning_rate = 0.01
@@ -23,66 +24,63 @@ n_train = 60000
 n_test = 10000
 
 # Step 1: Read in data
-mnist_folder = 'data/mnist'
+mnist_folder = 'data/{0}'.format(sys.argv[1])
 utils.download_mnist(mnist_folder)
 train, val, test = utils.read_mnist(mnist_folder, flatten=True)
 
 # Step 2: Create datasets and iterator
-# create training Dataset and batch it
+print('Create training Dataset and batch it...')
 train_data = tf.data.Dataset.from_tensor_slices(train)
-train_data = train_data.shuffle(10000) # if you want to shuffle your data
+train_data = train_data.shuffle(10000)  # if you want to shuffle your data
 train_data = train_data.batch(batch_size)
 
-# create testing Dataset and batch it
-test_data = None
-#############################
-########## TO DO ############
-#############################
-
+print('create testing Dataset and batch it...')
+test_data = tf.data.Dataset.from_tensor_slices(test)
+# not point to shuffle
+test_data = test_data.batch(batch_size)
 
 # create one iterator and initialize it with different datasets
-iterator = tf.data.Iterator.from_structure(train_data.output_types, 
-                                           train_data.output_shapes)
+iterator = tf.data.Iterator.from_structure(
+    train_data.output_types,
+    train_data.output_shapes
+)
+
 img, label = iterator.get_next()
 
-train_init = iterator.make_initializer(train_data)	# initializer for train_data
-test_init = iterator.make_initializer(test_data)	# initializer for train_data
+# initializer for train_data
+train_init = iterator.make_initializer(train_data)
+# initializer for test data
+test_init = iterator.make_initializer(test_data)
 
 # Step 3: create weights and bias
 # w is initialized to random variables with mean of 0, stddev of 0.01
 # b is initialized to 0
 # shape of w depends on the dimension of X and Y so that Y = tf.matmul(X, w)
 # shape of b depends on Y
-w, b = None, None
-#############################
-########## TO DO ############
-#############################
-
+num_classes = 10                # MNIST: 0 to 9
+w = tf.get_variable(
+    name='weights',
+    shape=(img.shape[1].value, num_classes),  # shape[0] is the batch_size
+    initializer=tf.random_normal_initializer(0, 0.01))
+b = tf.get_variable(
+    name='bias',
+    shape=(1, num_classes),
+    initializer=tf.zeros_initializer())
 
 # Step 4: build model
 # the model that returns the logits.
 # this logits will be later passed through softmax layer
-logits = None
-#############################
-########## TO DO ############
-#############################
-
+logits = tf.matmul(img, w) + b
 
 # Step 5: define loss function
 # use cross entropy of softmax of logits as the loss function
-loss = None
-#############################
-########## TO DO ############
-#############################
-
+entropy = tf.nn.softmax_cross_entropy_with_logits(
+    labels=label, logits=logits, name="entropy")
+loss = tf.reduce_mean(entropy, name="loss")
 
 # Step 6: define optimizer
 # using Adamn Optimizer with pre-defined learning rate to minimize loss
-optimizer = None
-#############################
-########## TO DO ############
-#############################
-
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 # Step 7: calculate accuracy with test set
 preds = tf.nn.softmax(logits)
@@ -91,13 +89,14 @@ accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
 
 writer = tf.summary.FileWriter('./graphs/logreg', tf.get_default_graph())
 with tf.Session() as sess:
-   
+
     start_time = time.time()
     sess.run(tf.global_variables_initializer())
 
     # train the model n_epochs times
-    for i in range(n_epochs): 	
-        sess.run(train_init)	# drawing samples from train_data
+    for i in range(n_epochs):
+        # drawing samples from train_data
+        sess.run(train_init)
         total_loss = 0
         n_batches = 0
         try:
@@ -107,7 +106,7 @@ with tf.Session() as sess:
                 n_batches += 1
         except tf.errors.OutOfRangeError:
             pass
-        print('Average loss epoch {0}: {1}'.format(i, total_loss/n_batches))
+        print('Average loss epoch {0}: {1}'.format(i, total_loss / n_batches))
     print('Total time: {0} seconds'.format(time.time() - start_time))
 
     # test the model
